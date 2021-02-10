2bb5dadd808648fe7d6658673b07fc085cc7f8a5
蒋金秋 <978472595@qq.com>
2021-02-10 20:57:29 +0800

    02：用物理组件制作平台（Joint2D)
    - 利用 Joint 2D 系列 Component 组件制作各种平台
    - 利用代码实现平台移动
    - 设计顶点销毁多余平台

---
+++ Assets/Scripts/FanPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanPlatform : MonoBehaviour
+{
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _animator.Play("Fan_Run");
+        }
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Vector3 _movement;
+    public float speed;
+    private GameObject _topLine;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _movement.y = speed;
+        _topLine = GameObject.Find("TopLine");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        transform.position += _movement * Time.deltaTime;
+        if (transform.position.y >= _topLine.transform.position.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
