17a3df1152ef1332252944389b3e316eb144ba48
蒋金秋 <978472595@qq.com>
2021-02-10 20:05:21 +0800

    01：项目介绍&场景搭建
    - 介绍本系列游戏项目
    - 3D Object Quad 制作动态背景

---
+++ Assets/Scripts/AnimationBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBackground : MonoBehaviour
+{
+    private Material _material;
+    private Vector2 _movement;
+
+    public Vector2 speed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _movement += speed * Time.deltaTime;
+        _material.mainTextureOffset = _movement;
+    }
+}
