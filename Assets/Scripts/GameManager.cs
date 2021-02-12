610b63fb9ac2e3fb94f8fd8368e41edc92bc2a10
蒋金秋 <978472595@qq.com>
2021-02-12 19:49:23 +0800

    06:游戏管理&UI(GameManager)
    - 设置 GameManager 来管理游戏的重新开始或推出
    - 添加简单 UI 实现结束画面
    - 调整得分 与 场景重载后时间对等

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public Text timeScore;
+    public GameObject gameOverUI;
+
+    private void Awake()
+    {
+        if (_instance != null)
+            Destroy(gameObject);
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeScore.text = Time.timeSinceLevelLoad.ToString("00");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void GameOver(bool isDead)
+    {
+        if (isDead)
+        {
+            _instance.gameOverUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -62,6 +62,7 @@ public class PlayerController : MonoBehaviour
     public void PlayerDead()
     {
         _isPlayerDead = true;
+        GameManager.GameOver(_isPlayerDead);
     }
 
     private void OnDrawGizmosSelected()
