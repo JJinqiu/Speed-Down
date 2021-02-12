843fd8664be6e86d92b273ae66844c568b5177dc
蒋金秋 <978472595@qq.com>
2021-02-12 16:52:16 +0800

    04：LineRenderer组件 & 一些小调整
    - 如何使用 Line Renderer 组件为Spike Ball添加一条连接线
    - 调整摄像机
    - 修正头顶尖刺的模糊

---
+++ Assets/Scripts/LineRender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRender : MonoBehaviour
+{
+    private LineRenderer _line;
+    public Transform startPoint;
+    public Transform endPoint;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _line.SetPosition(0, startPoint.position);
+        _line.SetPosition(1, endPoint.position);
+    }
+}
