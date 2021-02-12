a3eff0f8a50ced24f3880432b37bccfb88d07f92
蒋金秋 <978472595@qq.com>
2021-02-12 19:23:02 +0800

    05.随机生成平台&代码(Spawn Platforms)
    - 设计思路
    - 设置随机范围数字控制生成平台
    - 如何调整游戏可玩性

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> platforms = new List<GameObject>();
+
+    public float spawnTime;
+    private float _countTime;
+    private Vector3 _spawnPosition;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        SpawnPlatform();
+    }
+
+    private void SpawnPlatform()
+    {
+        _countTime += Time.deltaTime;
+        _spawnPosition = transform.position;
+        _spawnPosition.x = Random.Range(-3.5f, 3.5f);
+
+        if (_countTime >= spawnTime)
+        {
+            CreatePlatform();
+            _countTime = 0;
+        }
+    }
+
+    private void CreatePlatform()
+    {
+        var index = Random.Range(0, platforms.Count);
+        var newPlatform = Instantiate(platforms[index], _spawnPosition, Quaternion.identity);
+        newPlatform.transform.SetParent(gameObject.transform);
+    }
+}
